d492384b40aa8df844ad02432d610516aed80f72
darleneDiPierro <rosequinzley@gmail.com>
2023-03-09 19:05:48 +0700

    parallax

--- src/components/Paintings/Pantings.jsx
+++ src/components/Paintings/Pantings.jsx
@@ -11,10 +11,12 @@ const Pantings = () => {
             <img src={images.vangogh} alt="vangogh" className='vangogh' />
             <img src={images.mountain} alt="mountain" className='mountain' />
             <img src={images.city} alt="city" className='city' />
+            <p className='paper-1'>I guess i’m a visioner type of person <br /> hmm maybe ? but i just dream a lot. </p>
+            <p className='paper-2'>Like a lot .......... (is it bad ? T_T)</p>
         </div>
           <div className='parallax-main'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum porro a nihil sunt quasi nesciunt dicta dignissimos ab, reiciendis magnam dolorem laboriosam, vero atque recusandae veritatis, quae vel. Possimus, et?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum porro a nihil sunt quasi nesciunt dicta dignissimos ab, reiciendis magnam dolorem laboriosam, vero atque recusandae veritatis, quae vel. Possimus, et?</p>
+            <p>I always try to make a fun way to do my work or tasks <br /> so that i could do these things with all my heart xD</p>
+            <p>I often use my own fantasy or imagination to make such a heavy or boring tasks become more fun and exciting :)</p>
           </div>
     </div>
   )
